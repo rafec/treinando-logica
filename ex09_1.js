1d3687bf03f533f0e0cb94d812894b9119980987
rafec <rafael.brizuena@gmail.com>
2023-02-23 18:41:00 -0400

    improved code, with template literals

--- ex09_1.js
+++ ex09_1.js
@@ -1,8 +1,8 @@
 const prompt = require("prompt-sync")()
 
 function converterEmHoras(num){
-    console.log("Número em minutos: ", num)
-    console.log("Número em horas: ", num / 60)
+    console.log(`Número em minutos: ${num}`)
+    console.log(`Número em horas: ${num/60}`)
 }
 
 let num = Number(prompt("Digite um número em minutos: "))
