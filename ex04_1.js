a34148e578a1fa44edddfd23c60adf86ce7d1251
rafec <rafael.brizuena@gmail.com>
2023-02-22 20:25:39 -0400

    added ex04

---
+++ ex04_1.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")()
+
+function multiplica(num1, num2){
+    console.log("A multiplicação dos dois números é: ", num1 * num2)
+}
+
+let num1 = Number(prompt("Digite o primeiro número: "))
+let num2 = Number(prompt("Digite o segundo número: "))
+
+multiplica(num1, num2)
