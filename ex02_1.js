6a697fdd6ece59ebcb687ab5980f7e2643c81d19
rafec <rafael.brizuena@gmail.com>
2023-02-22 20:07:20 -0400

    added ex02

---
+++ ex02_1.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")()
+
+let num1 = prompt("Digite o primeiro número: ")
+let num2 = prompt("Digite o segundo número: ")
+
+let res = num1 * num2
+
+console.log("O resultado da multiplicação é: ", res)
