712dd442c5d8ae8668bbb18c32dc19188fff3e84
rafec <rafael.brizuena@gmail.com>
2023-02-23 18:34:47 -0400

    improved code

--- ex08_1.js
+++ ex08_1.js
@@ -1,8 +1,9 @@
 const prompt = require("prompt-sync")()
 
 function tabuada(num){
-    for (let i = 0; i < 10; i++){
-        console.log(num*(i+1))
+    for (let i = 1; i < 10; i++){
+        // console.log(i, " X ", num, " = ", num*i)
+        console.log(`${i} X ${num} = ${i*num}`)
     }
 }
 
