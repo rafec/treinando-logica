de002817768ea5f5c85f2930b2cabdc70c940fd0
rafec <rafael.brizuena@gmail.com>
2023-02-26 11:43:19 -0400

    added decimal verification for the number in hours

--- ex09_1.js
+++ ex09_1.js
@@ -1,10 +1,14 @@
 const prompt = require("prompt-sync")()
 
-function converterEmHoras(num){
-    console.log(`Número em minutos: ${num}`)
-    console.log(`Número em horas: ${num/60}`)
+function converterEmHoras(num) {
+    const convertido = num / 60
+
+    if (Number.isInteger(convertido)) {
+        return `Número em minutos: ${num}min\nNúmero em horas: ${convertido}h`
+    }
+        return `Número em minutos: ${num}min\nNúmero em horas: ${convertido.toFixed(2)}h`
 }
 
 let num = Number(prompt("Digite um número em minutos: "))
 
-converterEmHoras(num)
+console.log(converterEmHoras(num))
